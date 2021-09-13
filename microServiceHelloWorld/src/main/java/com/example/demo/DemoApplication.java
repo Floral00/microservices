f992e233fde21b7ef07f626d6cb7ea51179f34a1
Florent <floral0011@gmail.com>
2021-09-13 08:14:03 +0200

    debut courq

--- microServiceHelloWorld/src/main/java/com/example/demo/DemoApplication.java
+++ microServiceHelloWorld/src/main/java/com/example/demo/DemoApplication.java
@@ -19,7 +19,8 @@ public class DemoApplication {
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
+	
 
 }
-            
+
             
